989353a65b482a9f76c04d1a80b59df698ff9c6e
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-09-16 21:09:09 +0000

    load db config from db.env

--- resources/owncloud-start.sh
+++ resources/owncloud-start.sh
@@ -5,7 +5,7 @@ cat > /var/www/html/owncloud/config/autoconfig.php << EOF
   "dbname" => "$OWNCLOUD_DB_NAME",
   "dbuser" => "$OWNCLOUD_DB_USERNAME",
   "dbpass" => "$OWNCLOUD_DB_PASSWORD",
-  "dbhost" => "$OWNCLOUD_DB_HOST",
+  "dbhost" => "db",
   "dbtableprefix" => "",
 );
 EOF
