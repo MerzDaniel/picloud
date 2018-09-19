aaa1ffa6d6b8c0cf451c4382aac5da84b0b1acef
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-09-19 17:25:48 +0000

    mysql config - now a real db is used

--- resources/owncloud-start.sh
+++ resources/owncloud-start.sh
@@ -1,4 +1,6 @@
 cat > /var/www/html/owncloud/config/autoconfig.php << EOF
+# sudo chown www-data:www-data /mnt/data
+
 <?php
 \$AUTOCONFIG = array(
   "dbtype" => "$OWNCLOUD_DB_TYPE",
@@ -6,7 +8,17 @@ cat > /var/www/html/owncloud/config/autoconfig.php << EOF
   "dbuser" => "$OWNCLOUD_DB_USERNAME",
   "dbpass" => "$OWNCLOUD_DB_PASSWORD",
   "dbhost" => "db",
-  "dbtableprefix" => "",
+# "datadirectory" doesn't seem to work? 
+#  "datadirectory" => "/mnt/data",
+  "mysql.utf8mb4" => true,
+  "passwordsalt" => "$OWNCLOUD_PASSWORDSALT",
+  "memcache.distributed" => "\\OC\\Memcache\\Redis",
+  "redis" => [
+    "host" => "redis",
+    "port" => 6379,
+    "timeout" => 0.0,
+  ],
+  "files_external_allow_create_new_local" => "true",
 );
 EOF
 
