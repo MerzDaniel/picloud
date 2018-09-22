55d914ad93fa3f9377e9e1300d46bc007dd679f1
Daniel Merz <MerzDanielDeveloper@gmail.com>
2018-09-22 07:32:21 +0000

    use created oc external volume id

--- resources/add_external_volumes.sh
+++ resources/add_external_volumes.sh
@@ -9,14 +9,14 @@ echo ${volume_ids[*]}
 echo $volume_names
 for i in "${!volume_paths[@]}"; do
 	volume_path=${volume_paths[$i]}
-	oc_volume_id=${volume_ids[$i]}
 	oc_volume_name=${volume_names[$i]}
 
 	echo "load external volume $volume_path"
 #        echo $volume_path $oc_volume_id $oc_volume_name
 
 	sudo -u $htuser $ocpath/occ app:enable files_external
-	sudo -u $htuser $ocpath/occ files_external:create $oc_volume_name \\OC\\Files\\Storage\\Local null::null
+	resArr=($( sudo -u $htuser $ocpath/occ files_external:create $oc_volume_name \\OC\\Files\\Storage\\Local null::null ))
+	oc_volume_id=${resArr[4]}
 	sudo -u $htuser $ocpath/occ files_external:config $oc_volume_id datadir $volume_path
 	sudo -u $htuser $ocpath/occ files_external:option $oc_volume_id enable_sharing true
 	sudo -u $htuser $ocpath/occ files_external:applicable --add-user=$ocuser $oc_volume_id
